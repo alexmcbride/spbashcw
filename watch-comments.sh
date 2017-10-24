ec05f49f5d01e5912511182873d38d21ff9c104d
Alex McBride <xandermcbride@gmail.com>
2017-10-24 10:14:42 +0100

    Updated

--- watch-comments.sh
+++ watch-comments.sh
@@ -1,32 +1,44 @@
 #! /bin/bash
+#
+# Bash Coursework - script for watch command
+# Author: Alex McBride
+# Student ID: S1715224
+# Student Email: AMCBRI206@caledonian.ac.uk
+# Date: 22/10/2017
 
 USAGE="Usage: $0 [dir] | [-k]"
 watch_pid=
 
+# Starts watching specified directory
 start_watch()
 {
 	watch -n 15 ls $1 &
-	watch_pid=$!
+	watch_pid=$! # Save PID so we can kill it later
 
+	# Keep process running
 	while true
 	do
 		sleep 1
 	done	
 }
 
+# Stops any running watch scripts.
 stop_watch()
 {
 	pids=($(ps -ef | awk '/[w]atch.sh/{print $2}'))
 	for pid in $pids
 	do
+		# Ignore this script.
 		if [[ $pid -ne $$ ]]
 		then
+			# Stop script
 			echo "Stopping watch.sh script (PID: $pid)"
 			kill $pid
 		fi
 	done
 }
 
+# Make sure watch process is always killed when we exited
 handle_trap()
 {
 	if [[ -n $watch_pid ]]
@@ -37,6 +49,7 @@ handle_trap()
 
 trap handle_trap EXIT
 
+# Handle options
 while getopts :k args
 do
 	case $args in
@@ -47,9 +60,11 @@ do
 	esac
 done		
 
+# Removed processed options
 ((pos = OPTIND - 1))
 shift $pos 
 
+# Handle main command
 if (( $# == 1 ))
 then 
 	start_watch $1
